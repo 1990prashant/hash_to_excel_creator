de77684073a9488940ef619751a15f027104211c
Prashant <prashant@threadsol.com>
2018-01-09 00:15:24 +0530

    Fixed issues

--- lib/hash_to_excel_creator.rb
+++ lib/hash_to_excel_creator.rb
@@ -33,7 +33,7 @@ class HashToExcelCreator
 
     end
     stream = @package.to_stream()
-    file_name = File.expand_path('../lib/sample.xlsx', __FILE__)
+    file_name = File.expand_path('../lib/sample.xlsx')
     File.open(file_name, 'w+') { |f| f.write(stream.read) }
   end
 
