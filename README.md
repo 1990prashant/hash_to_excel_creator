75c6769eb6c39dd55b7a6515934d1c4e9cf68b89
Prashant <prashant@threadsol.com>
2018-01-09 14:11:13 +0530

    Fixed with array of hashesh with sheet_name

--- README.md
+++ README.md
@@ -11,11 +11,12 @@ gem 'hash_to_excel_creator', github: '1990prashant/hash_to_excel_creator'
 Create a new object of HashToExcelCreator Class and call get_excel() method
 
 hte = HashToExcelCreator.new
-hte.get_excel(hash)
+hte.get_excel(hash, file_name)
 
 # Example hash
 
 {
+  sheet_name: "Name of your sheet"
   a: [1, 2], 
   b: [1, 3]
 }
--- lib/hash_to_excel_creator.rb
+++ lib/hash_to_excel_creator.rb
@@ -21,31 +21,43 @@ class HashToExcelCreator
 
   def convert_hash_to_excel(hash, user_file_name)
     if hash.is_a? Hash
-      @workbook.add_worksheet(name: "Sheet-1") do |sheet|
-        # add_heading(hash.keys, sheet)
-        # add_values(hash.values, sheet)
-        sheet.add_row(hash.keys.flatten)
-        get_final_values(hash.values).each do |arr|
-          sheet.add_row(arr.flatten)
-        end
-      end
+      create_excel(hash)
     elsif hash.is_a? Array
-
+      hash.each_with_index do |obj, (index + 1)|
+        create_excel(convert_to_symbolize_hash(obj), index)
+      end
     end
     stream = @package.to_stream()
     file_name = File.expand_path("../#{user_file_name}.xlsx")
     File.open(file_name, 'w+') { |f| f.write(stream.read) }
   end
 
-  def add_heading(headings, sheet)
-    append_row(headings, sheet)
+  def convert_to_symbolize_hash(obj)
+    hash = {}
+    obj.map{|key, value| hash[key.to_sym] = value}
+    return hash
   end
 
-  def add_values(values_arr, sheet)
-    get_final_values(values_arr).each do |arr|
-      append_row(arr, sheet)
+  def create_excel(hash, sheet_number = 1)
+
+    sheet_name = hash.has_key?(:sheet_name) ? hash[:sheet_name] : "Sheet-#{sheet_number}"
+    @workbook.add_worksheet(name: sheet_name) do |sheet|
+      sheet.add_row(hash.except(:sheet_name).keys.flatten)
+      get_final_values(hash.except(:sheet_name).values).each do |arr|
+        sheet.add_row(arr.flatten)
+      end
     end
   end
+
+  # def add_heading(headings, sheet)
+  #   append_row(headings, sheet)
+  # end
+
+  # def add_values(values_arr, sheet)
+  #   get_final_values(values_arr).each do |arr|
+  #     append_row(arr, sheet)
+  #   end
+  # end
 
   def get_final_values(values_arr)
     result_arr = []
